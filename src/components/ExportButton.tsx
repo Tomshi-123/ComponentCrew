0e1b1e0829d4daf062f369965cad1d7980fb715b
Erik Eriksson Broomé <erik.o.eriksson@telia.com>
2025-09-23 16:10:38 +0200

    added logic code to pdfUtils. added Button code to ExportButton component.

--- src/components/ExportButton.tsx
+++ src/components/ExportButton.tsx
@@ -1,13 +1,25 @@
+import { Button } from "@mui/material";
+import { exportDataToPDF } from "../utils/pdfUtils";
 import type { TableData } from "../types/Types";
 
 type ButtonProps = {
   data: TableData;
 };
 
-export default function Button({ data }: ButtonProps) {
+export default function ExportButton({ data }: ButtonProps) {
   // Knapp för export till PDF
   // och eventuellt till Ecxel
   // Använder funktioner från /utils/pdfUtils.ts och /utils/excelUtils.ts.
 
-  return <div></div>;
+  const handleExport = () => {
+    exportDataToPDF(data, "sheet-data.pdf");
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleExport}>
+        Export to PDF
+      </Button>
+    </div>
+  );
 }
