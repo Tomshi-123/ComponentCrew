b43eb04243e952f933fc853b4cf167393ea4e8a6
alexander <alexander.t.simonsen@gmail.com>
2025-10-02 13:44:57 +0200

    Rename Main to Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Layout from "./layout/Layout";
 import ExportButton from "./components/ExportButton";
-import Main from "./components/Main";
+import Main from "./components/Dashboard";
 
 import "./App.css";
 
